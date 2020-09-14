112e9628787d2a001a9ee447d9bece99e90cb191
Nycolas Felipe Lourenço <nycolaslourenco.contato@gmail.com>
2020-09-14 19:04:03 -0300

    Última versão que modifiquei

---
+++ site-skyrim-2/scripts/funcoes.js
@@ -0,0 +1,21 @@
+﻿function mudarImagem (imagem){
+	document.getElementById("logo-imagem").src = imagem;
+}
+
+function show () {
+    var element = document.getElementById("frame-mapa");
+    element.classList.remove("esconder");
+    element.classList.add("exibir");
+
+    var element = document.getElementById("botao-min")
+    element.classList.add("exibir");
+
+    var element = document.getElementById("ajuda-botao")
+    element.classList.add("exibir");
+}
+
+function esconder () {
+	var element = document.getElementById("frame-mapa");
+    element.classList.add("esconder");
+    element.classList.remove("exibir");
+}
